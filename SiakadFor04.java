b8bd66e7c993c82eef5421f217cdca2560fee2bd
sopyanbadawi <sopyanbadawi.com>
2024-10-09 10:03:38 +0700

    Siakad 2

--- SiakadFor04.java
+++ SiakadFor04.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
     
     Scanner sc = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah = 100;
+    int lulus = 0, tdkLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,8 +16,15 @@ public static void main(String[] args) {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tdkLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + lulus);
+        System.out.println("Jumlah Mahasiswa tidak Lulus: " + tdkLulus);
     }
 }
