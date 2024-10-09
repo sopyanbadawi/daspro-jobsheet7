ce6b37ee889fd7d11d9a23a89a6cd9044a36af9c
sopyanbadawi <sopyanbadawi.com>
2024-10-09 10:56:12 +0700

    Siakad While 2

--- SiakadWhile04.java
+++ SiakadWhile04.java
@@ -21,6 +21,7 @@ public class SiakadWhile04 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Pesan: Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <=73 ) {
